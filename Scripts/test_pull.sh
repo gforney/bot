14e57caa4c6d6e94cad59b9a36ae9e0e5cccb1db
gforney <gforney@gmailcom>
2025-10-01 17:21:15 -0400

    scripts: add option to test_pull.sh script to clean and update repos

--- Scripts/test_pull.sh
+++ Scripts/test_pull.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 id=$1
 repo=$2
+cleanupdate=$3
 
+CURDIR=`pwd`
 if [ "$repo" == "" ]; then
   repo=smv
 fi
+if [ "$cleanupdate" != "" ]; then
+  echo "cleaning repos"
+  ./clean_repos.sh     >& /dev/null
+  echo "updating repos"
+  ./update_repos.sh -m >& /dev/null
+fi
 
-CURDIR=`pwd`
-cd ../../$repo
-
+cd $CURDIR/../../$repo
 
 git fetch firemodels pull/$id/head:PR_$id
 git branch -a
