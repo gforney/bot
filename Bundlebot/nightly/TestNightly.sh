a4c9c83489fe922004477a1677d3a653e31d2d53
gforney <gforney@gmail.com>
2026-08-29 20:05:06 -0400

    bundle script: correction to script that determines whether to build  bundle

--- Bundlebot/nightly/TestNightly.sh
+++ Bundlebot/nightly/TestNightly.sh
@@ -4,9 +4,9 @@ case "$(uname -s)" in
   Linux*)  OS=linux ;;
   Darwin*) OS=osx ;;
 esac
-SUFFIX=lnx
+SUFFIX=nightly_lnx
 if [ "$OS" == "osx" ]; then
- SUFFIX=osx
+ SUFFIX=_nightly_osx
 fi
 
 INFO=FDS_INFO.txt
@@ -15,16 +15,12 @@ rm -f "$INFO" "$TOC"
 gh release download FDS_TEST -p "$INFO" -D . -R github.com/firemodels/test_bundles
 FDS_REVISION=$(awk '$1 == "FDS_REVISION" {print $2; exit}' "$INFO")
 SMV_REVISION=$(awk '$1 == "SMV_REVISION" {print $2; exit}' "$INFO")
-PLATFORM=$SUFFIX
-BUNDLENAME="${FDS_REVISION}_${SMV_REVISION}_${PLATFORM}.sh"
-gh release view FDS_TEST -R github.com/firemodels/test_bundles | grep nightly_$SUFFIX | awk '{print $2}' > $TOC
+BUNDLENAME="${FDS_REVISION}_${SMV_REVISION}_${SUFFIX}.sh"
+gh release view FDS_TEST -R github.com/firemodels/test_bundles | grep $SUFFIX | awk '{print $2}' > $TOC
 
 while IFS= read -r filename; do
   if [[ "$filename" == $BUNDLENAME ]]; then
-   echo bundle exists
     exit 1
   fi
 done < "$TOC"
-echo bundle does not exist
 exit 0
- 
